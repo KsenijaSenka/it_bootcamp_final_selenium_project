f7ecc97e5692039ec0aae541c2567f895ebd7a2b
Ksenija Beocanin <ksenijabeocanin@gmail.com>
2023-10-16 09:53:04 +0200

    Created BasicTest

---
+++ src/test/java/tests/BasicTest.java
@@ -0,0 +1,42 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.IOException;
+import java.time.Duration;
+
+public abstract class BasicTest {
+    protected String baseUrl = "https://vue-demo.daniel-avellaneda.com";
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected JavascriptExecutor js;
+
+    @BeforeClass
+    public void BeforeClass() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    @BeforeMethod
+    public void setup() {
+        driver.manage().deleteAllCookies();
+        driver.navigate().to(baseUrl);
+    }
+    @AfterMethod
+    public void afterMethod(ITestResult testResult) throws IOException {
+        js.executeScript("window.localStorage.clear();");
+        if (testResult.getStatus() == ITestResult.FAILURE) {
+            Helper.takeAScreenshot(driver, "screenshots/" + testResult.getName() + ".jpg");
+        }
+    }
+}
