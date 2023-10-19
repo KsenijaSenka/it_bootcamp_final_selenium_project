ae7745a5e037f88254dfe5efa5662aaad6e795fb
Ksenija Beocanin <ksenijabeocanin@gmail.com>
2023-10-19 20:04:40 +0200

    Set locale to EN

--- src/test/java/tests/LocaleTests.java
+++ src/test/java/tests/LocaleTests.java
@@ -6,10 +6,17 @@ import retry.RetryAnalyzer;
 
 public class LocaleTests extends BasicTest{
     @Test(priority = 1, retryAnalyzer = RetryAnalyzer.class)
-    public void setLocaleToES(){
+    public void setLocaleToES() {
         navPage.clickOnLanguagesButton();
         navPage.clickEsLanguageFromDropdown();
-        Assert.assertEquals(navPage.getHeaderText(),"Página de aterrizaje",
+        Assert.assertEquals(navPage.getHeaderText(), "Página de aterrizaje",
                 "Header text should be Página de aterrizaje");
+    }
+    @Test(priority = 2, retryAnalyzer = RetryAnalyzer.class)
+    public void setLocaleToEN() {
+        navPage.clickOnLanguagesButton();
+        navPage.clickEnLanguageFromDropdown();
+        Assert.assertEquals(navPage.getHeaderText(), "Landing",
+                "Header text should be Landing");
     }
 }
