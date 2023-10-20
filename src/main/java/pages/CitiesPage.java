e06cc6e726d63dfcf8c1e90488b1851e9b3773d6
Ksenija Beocanin <ksenijabeocanin@gmail.com>
2023-10-20 19:15:11 +0200

    Added Admin Cities Tests

--- src/main/java/pages/CitiesPage.java
+++ src/main/java/pages/CitiesPage.java
@@ -1,12 +1,13 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class CitiesPage extends BasicPage{
+public class CitiesPage extends BasicPage {
     public CitiesPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
     }
@@ -30,23 +31,73 @@ public class CitiesPage extends BasicPage{
     public void enterCityOfCurrentUser(String city) {
         getCityInput().sendKeys(city);
     }
-    public void clickToSaveCity(){
+
+    public void clickToSaveCity() {
         driver.findElement(By.className("btnSave")).click();
     }
-    public void waitCreateEditDialog(){
+
+    public void waitCreateEditDialog() {
         wait
                 .withMessage("Create and Edit Dialogue is not visible.")
                 .until(ExpectedConditions.visibilityOfElementLocated(
                         By.xpath("//div[contains(@class, 'dlgNewEditItem')]")));
     }
-    public void waitForSucessDialog(){
+
+    public void waitForSuccessDialog() {
         wait
                 .withMessage("Save Dialogue is not visible.")
                 .until(ExpectedConditions.visibilityOfElementLocated(
-                By.xpath("//div[contains(@class, 'success')]")));
+                        By.xpath("//div[contains(@class, 'success')]")));
     }
+
     public String getMessageFromSuccessPopUpText() {
         return driver.findElement(By.xpath(
                 "//div[contains(@class, 'success')]")).getText();
+    }
+
+    public WebElement getSearchInput() {
+        return driver.findElement(By.id("search"));
+    }
+
+    public void clearAndEnterCityInSearchInput(String city) {
+        getSearchInput().sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        getSearchInput().sendKeys(city);
+    }
+
+    public void waitForNumberOfRows(int rowsNo) {
+        wait.until(ExpectedConditions.numberOfElementsToBe
+                (By.xpath("//div[@class='v-data-table__wrapper']//tbody/tr"), rowsNo));
+    }
+
+    public void clickEditButtonForRow(int rowsNo) {
+        driver.findElement(By.xpath("//tr[" + rowsNo + "]/td[1]/div/button[@id='edit']")).click();
+    }
+
+    public void clearAndEnterCityOfCurrentUser(String city) {
+        getCityInput().sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        getCityInput().sendKeys(city);
+    }
+    public String getCityNameText(){
+        return driver.findElement(By.xpath("//*[@id='app']//tr[1]/td[2]")).getText();
+    }
+
+    public void clickDeleteButtonForRow(int rowsNo) {
+        driver.findElement(By.xpath("//tr[" + rowsNo  + "]/td[1]/div/button[@id='delete']")).click();
+    }
+    public void waitDeleteDialogueVisibility(){
+         wait.until(ExpectedConditions.visibilityOfElementLocated
+                (By.xpath("//div[contains(@class, 'v-dialog')]//header[contains(@class, 'warning')]")));
+    }
+    public WebElement getCell(int row, int column){
+        return driver.findElement(By.xpath("//table/tbody/tr["+row+"]/td["+column+"]"));
+    }
+    public String getCellText(int row, int column){
+        return getCell(row, column).getText();
+    }
+    public WebElement getDeleteButtonFromDialogue() {
+        return driver.findElement(By.xpath("//*[@id='app']//div[2]/button[2]"));
+    }
+    public void clickDeleteButtonFromDialogue(){
+        getDeleteButtonFromDialogue().click();
     }
 }
--- src/test/java/tests/AdminCitiesTests.java
+++ src/test/java/tests/AdminCitiesTests.java
@@ -50,8 +50,67 @@ public class AdminCitiesTests extends BasicTest{
         citiesPage.waitCreateEditDialog();
         citiesPage.enterCityOfCurrentUser(city);
         citiesPage.clickToSaveCity();
-        citiesPage.waitForSucessDialog();
+        citiesPage.waitForSuccessDialog();
         Assert.assertTrue(citiesPage.getMessageFromSuccessPopUpText()
                 .contains("Saved successfully"), "Message is incorrect");
+    }
+
+    @Test(priority = 4, retryAnalyzer = RetryAnalyzer.class)
+    public void editCity() {
+        String oldCityName = "Ksenija Beocanin's city";
+        String newCityName = "Ksenija Beocanin's city Edited";
+        int rowsNo = 1;
+
+        navPage.clickAdminButton();
+        navPage.clickCitiesButton();
+        citiesPage.clearAndEnterCityInSearchInput(oldCityName);
+
+        citiesPage.waitForNumberOfRows(rowsNo);
+        citiesPage.clickEditButtonForRow(rowsNo);
+
+        citiesPage.getCityInput().clear();
+        citiesPage.getCityInput().sendKeys(newCityName);
+        citiesPage.clearAndEnterCityOfCurrentUser(newCityName);
+        citiesPage.clickToSaveCity();
+
+        citiesPage.waitForSuccessDialog();
+        Assert.assertTrue(citiesPage.getMessageFromSuccessPopUpText()
+                .contains("Saved successfully"), "Message is incorrect");
+    }
+
+    @Test(priority = 5, retryAnalyzer = RetryAnalyzer.class)
+    public void searchCity() {
+        String cityName = "Ksenija Beocanin's city Edited";
+        int rowsNo = 1;
+
+        navPage.clickAdminButton();
+        navPage.clickCitiesButton();
+        citiesPage.clearAndEnterCityInSearchInput(cityName);
+        citiesPage.waitForNumberOfRows(rowsNo);
+
+        Assert.assertEquals(citiesPage.getCityNameText(), cityName,
+                "The name of the city in the top row should be equal to searched city name.");
+    }
+
+    @Test(priority = 6, retryAnalyzer = RetryAnalyzer.class)
+    public void deleteCity() {
+        String cityName = "Ksenija Beocanin's city Edited";
+        int rowsNo = 1;
+
+        navPage.clickAdminButton();
+        navPage.clickCitiesButton();
+        citiesPage.clearAndEnterCityInSearchInput(cityName);
+        citiesPage.waitForNumberOfRows(rowsNo);
+
+        Assert.assertEquals(citiesPage.getCityNameText(), cityName,
+                "The name of the city in the top row should be equal to searched city name.");
+
+        citiesPage.clickDeleteButtonForRow(rowsNo);
+        citiesPage.waitDeleteDialogueVisibility();
+        citiesPage.clickDeleteButtonFromDialogue();
+
+        citiesPage.waitForSuccessDialog();
+        Assert.assertTrue(citiesPage.getMessageFromSuccessPopUpText()
+                .contains("Deleted successfully"), "Message is incorrect");
     }
 }
